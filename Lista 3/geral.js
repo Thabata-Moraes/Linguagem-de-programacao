cb9e745a4066e685ffe046f31fc49c215d30c287
Thabata Moraes <90910437+Thabata-Moraes@users.noreply.github.com>
2023-03-21 12:17:09 +0000

    aula 21/03

--- Lista 3/geral.js
+++ Lista 3/geral.js
@@ -47,3 +47,55 @@ function calcularEx01(){
 
 }
 
+function calcularEx03(){
+    let idade
+    let f1 = 0
+    let f2 = 0 
+    let f3 = 0
+    let f4 = 0 
+    let f5 = 0
+    let pf1 = 0 
+    let pf5 = 0
+
+    for (let i=0; i<8; i++){
+        idade = Number(prompt(`Informa a idade ${i+1}`))
+        if(idade>0 && idade<=15){
+            f1++
+        } else if(idade>15 && idade<=30){
+            f2++
+        } else if(idade>30 && idade<=45){
+            f3++
+        } else if(idade>45 && idade<=60){
+            f4++
+        } else if (idade>60){
+            f5++
+        } else {
+            alert('Idade Inválida')
+            break
+        }
+    }
+    pf1 = (f1*100)/8
+    pf5 = (f5*100)/8
+
+    let mensagem = `
+        A porcentagem de idades na faixa etária 1 foi de: ${pf1} </br>
+        A porcentagem de idades na faixa etária 5 foi de: ${pf5}
+    `
+    document.getElementById("mensagem").innerHTML =  mensagem
+
+    
+}
+
+function calcularEx04(){
+    let num = Number(prompt('Entre com um valor'))
+    let mensagem = ''
+    let tabuada
+    for (let i=0; i<=10; i++){
+        tabuada = num * (i)
+        mensagem = mensagem + `${num} * ${i} = ${tabuada} </br> `
+    }
+
+    document.getElementById("mensagem").innerHTML =  mensagem
+
+    
+}
